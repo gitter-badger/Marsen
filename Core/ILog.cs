032e211fefe4f6fac4a008a30ca2f39e18bea725
marklin <marklin@nine-yi.com>
2015-10-16 18:49:23 +0800

    Marsen
    這次一定要成功!!!

---
+++ Core/ILog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marsen.Utility.Core.Log
+{
+    /// <summary>
+    /// Log介面
+    /// </summary>
+    public interface ILog
+    {
+        /// <summary>
+        /// 記錄Log
+        /// </summary>
+        /// <param name="message">Log訊息</param>
+        /// <returns></returns>
+        public bool Log(string message);
+        /// <summary>
+        /// 記錄Log至指定路徑
+        /// </summary>
+        /// <param name="message">Log訊息</param>
+        /// <param name="savePath">Log存檔路徑</param>
+        /// <returns></returns>
+        public bool Log(string message,string savePath);
+    }
+}
