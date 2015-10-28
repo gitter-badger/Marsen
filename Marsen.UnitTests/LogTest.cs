db99c1d0191b7623b3b6447e79a2c8910accd0ff
marklin <marklin@nine-yi.com>
2015-10-28 18:33:30 +0800

    Add UnitTest Proj

--- Marsen.UnitTests/LogTest.cs
+++ Marsen.UnitTests/LogTest.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Marsen.Utility.Core.Log;
 
 namespace Marsen.UnitTests
 {
     [TestClass]
-    public class UnitTest1
+    public class LogTest
     {
         [TestMethod]
         public void TestMethod1()
         {
+            ILog tester = new Logger();
+            tester.Log("test");
         }
     }
 }
