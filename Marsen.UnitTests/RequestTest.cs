7d1a55a37b6a10182fbd313c628b933726fc5429
marklin <marklin@nine-yi.com>
2016-01-21 20:23:29 +0800

    [新增] 測試方法

--- Marsen.UnitTests/RequestTest.cs
+++ Marsen.UnitTests/RequestTest.cs
@@ -1,18 +1,29 @@
 ﻿using System;
+using Core.Request;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace Marsen.UnitTests
 {
     [TestClass]
     public class RequestTest
     {
+        private IRequest reqTester;
         [TestMethod]
         public void TestPost()
         {
+            var data  = new Dictionary<string, string>();
+
+            reqTester = new BaseRequest("http://www.google.com", data);
+            var resonpse = reqTester.Load();
         }
         [TestMethod]
         public void TestGet()
         {
+            var data = new Dictionary<string, string>();
+
+            reqTester = new BaseRequest("http://www.google.com", data,"GET");
+            var resonpse = reqTester.Load();
         }
     }
 }
