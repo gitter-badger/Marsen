0e979cdb2f2830de16d018f8512d986a44e0f289
marklin <marklin@nine-yi.com>
2016-02-16 18:32:07 +0800

    [新增] Demo page

---
+++ Marsen.WebSite/Controllers/DemoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Marsen.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult XSS() {
+            return View();
+        }
+    }
+}
