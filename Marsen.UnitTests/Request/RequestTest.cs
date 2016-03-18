e458ab0e23eca6ba04c8f572b14194ce6cb3a2cb
marklin <marklin@nine-yi.com>
2016-03-18 15:29:49 +0800

    [測試] - 撰寫Request 測試案例

--- Marsen.UnitTests/Request/RequestTest.cs
+++ Marsen.UnitTests/Request/RequestTest.cs
@@ -10,22 +10,48 @@ namespace Marsen.Core.Tests
     [TestClass]
     public class RequestTest
     {
-        private IRequest reqTester;
         [TestMethod]
-        public void TestPost()
+        public void 呼叫Request_Load後執行方法為Post的Request()
         {
+            ////arrage
+            string uri = "http://mockapi.marsen";
             var data = new Dictionary<string, string>();
-            //// Bing 僅是為了測用的URI，可以替換為其它的URI
-            reqTester = new BaseRequest("http://www.bing.com/", data);
-            var resonpse = reqTester.Load();
+            StubRequest target = Substitute.For<StubRequest>(uri, data, "POST", 30);
+            ////act
+            target.Load();
+            ////assert
+            target.Received().PostAsync();
         }
+
         [TestMethod]
-        public void TestGet()
+        public void 呼叫Request_Load後執行方法為Get的Request()
+
+
         {
+            ////arrage
+            string uri = "http://mockapi.marsen";
             var data = new Dictionary<string, string>();
-            //// Bing 僅是為了測用的URI，可以替換為其它的URI
-            reqTester = new BaseRequest("http://www.bing.com/", data, "GET");
-            var resonpse = reqTester.Load();
+            StubRequest target = Substitute.For<StubRequest>(uri, data,"GET",30);
+            ////act
+            target.Load();
+            ////assert
+            target.Received().GetAsync();
+        }
+    }
+    public class StubRequest : BaseRequest
+    {
+        public StubRequest(string uri ,Dictionary<string,string> data, string method = "POST", int timeout = 30) : base(uri, data , method , timeout)
+        { }
+
+
+        internal override async Task<string> PostAsync()
+        {
+            return null;
+        }
+
+        internal override async Task<string> GetAsync()
+        {
+            return null;
         }
     }
 }
