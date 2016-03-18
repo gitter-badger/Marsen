759e152b6e9c1bc30260f3833480991acc509957
marklin <marklin@nine-yi.com>
2016-03-18 15:39:10 +0800

    [重構] - 測試專案

--- Marsen.UnitTests/Request/RequestTest.cs
+++ Marsen.UnitTests/Request/RequestTest.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using Marsen.Core.Request;
+﻿using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using NSubstitute;
 
-namespace Marsen.Core.Tests
+namespace Marsen.Core.Tests.Request
 {
     [TestClass]
     public class RequestTest
@@ -26,32 +23,15 @@ namespace Marsen.Core.Tests
         [TestMethod]
         public void 呼叫Request_Load後執行方法為Get的Request()
 
-
         {
             ////arrage
             string uri = "http://mockapi.marsen";
             var data = new Dictionary<string, string>();
-            StubRequest target = Substitute.For<StubRequest>(uri, data,"GET",30);
+            StubRequest target = Substitute.For<StubRequest>(uri, data, "GET", 30);
             ////act
             target.Load();
             ////assert
             target.Received().GetAsync();
-        }
-    }
-    public class StubRequest : BaseRequest
-    {
-        public StubRequest(string uri ,Dictionary<string,string> data, string method = "POST", int timeout = 30) : base(uri, data , method , timeout)
-        { }
-
-
-        internal override async Task<string> PostAsync()
-        {
-            return null;
-        }
-
-        internal override async Task<string> GetAsync()
-        {
-            return null;
         }
     }
 }
---
+++ Marsen.UnitTests/Request/StubRequest.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Marsen.Core.Request;
+
+namespace Marsen.Core.Tests.Request
+{
+    public class StubRequest : BaseRequest
+    {
+        public StubRequest(string uri, Dictionary<string, string> data, string method = "POST", int timeout = 30) : base(uri, data, method, timeout)
+        { }
+
+        internal override async Task<string> PostAsync()
+        {
+            return null;
+        }
+
+        internal override async Task<string> GetAsync()
+        {
+            return null;
+        }
+    }
+}
