d540458a549afc4fe28b0caef1ef70178a6202e2
marklin <marklin@nine-yi.com>
2016-04-01 18:25:25 +0800

    [新增] add Notify Interface

---
+++ Core/Notify/INotify.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marsen.Core.Notify
+{
+    /// <summary>
+    /// INotify interface
+    /// </summary>
+    public interface INotify
+    {
+        /// <summary>
+        /// Send Notify
+        /// </summary>
+        /// <param name="message">message</param>
+        void Send(string message);
+    }
+}
