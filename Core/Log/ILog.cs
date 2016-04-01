08c166cb7c4eb6fe511de3b40aa4646f6ef5d458
marklin <marklin@nine-yi.com>
2016-04-01 17:39:14 +0800

    [測試] FileLogger 整合測試

--- Core/Log/ILog.cs
+++ Core/Log/ILog.cs
@@ -10,13 +10,6 @@
         /// </summary>
         /// <param name="message">Log訊息</param>
         /// <returns></returns>
-        bool Log(string message);
-        /// <summary>
-        /// 記錄Log至指定路徑
-        /// </summary>
-        /// <param name="message">Log訊息</param>
-        /// <param name="savePath">Log存檔路徑</param>
-        /// <returns></returns>
-        bool Log(string message,string savePath);
+        void Log(string message);
     }
 }
