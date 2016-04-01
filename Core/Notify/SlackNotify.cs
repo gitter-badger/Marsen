3a5803677c6b54a85278ecd9fce159ed9234c30a
marklin <marklin@nine-yi.com>
2016-04-01 18:55:22 +0800

    [新增] 加入Notify 測試方法(未實作)

---
+++ Core/Notify/SlackNotify.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marsen.Core.Notify
+{
+    /// <summary>
+    /// SlackNotify
+    /// </summary>
+    public class SlackNotify : INotify
+    {
+        /// <summary>
+        /// Send
+        /// </summary>
+        /// <param name="message">message</param>
+        public void Send(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Marsen.UnitTests/Notify/SlackNotifyTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Marsen.Core.Notify;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marsen.Core.Notify.Tests
+{
+    [TestClass()]
+    public class SlackNotifyTests
+    {
+        [TestMethod()]
+        public void SendTest()
+        {
+            ////arrange
+            ////act
+            ////assert
+            Assert.Fail();
+        }
+    }
+}
