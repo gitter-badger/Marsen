2410e9120abbfdd3180e245aa906fdccc3c4298f
marklin <marklin@nine-yi.com>
2016-04-01 17:53:48 +0800

    [修正] 簡單工廠不應該在測試專案

--- Core/Request/RequsetFactory.cs
+++ Core/Request/RequsetFactory.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Marsen.Core.Request;
 
-namespace Marsen.Core.Tests.Request
+namespace Marsen.Core.Request
 {
     public class RequsetFactory
     {
